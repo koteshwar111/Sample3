d7cad924373afcfe31e1c1a6b75587dd097007e8
koteshwar111 <163884974+koteshwar111@users.noreply.github.com>
2024-12-08 14:09:00 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,5 @@
-class Helloworld{
-    public static void mian(String [] args){
-        System.out.println("hello-world");
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
